8ff032726af5cbbf6c3c00b733b572d2c4bf7e29
Ibrohim <ibrohimjonodilov28@gmail.com>
2023-05-31 10:46:12 +0500

    check number

--- calculator.dart
+++ calculator.dart
@@ -1,3 +1,15 @@
+import 'dart:io';
+
 void main(List<String> args) {
-  print("object");
+  print("It is our calculator");
+}
+
+int? isNumber(String output) {
+  stdout.write(output);
+  int? number = int.tryParse(stdin.readLineSync() ?? '');
+  if (number is num) {
+    return number;
+  }
+  stdout.write('Xato kiritildi qayta urining: \n');
+  return isNumber(output);
 }
